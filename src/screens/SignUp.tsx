b1d3bee107a466b93daf78af382eb9c01b2e6472
Carlos Neto <cneto2001@hotmail.com>
2024-10-07 13:06:22 -0300

    Fetch API

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -41,8 +41,17 @@ export function SignUp() {
         navigation.goBack();
     }
 
-    function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-        console.log({ name, email, password, password_confirm });
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+        const response = await fetch('http://192.168.0.121/users', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+        const data = await response.json();
+        console.log(data);
     }
 
     return (
